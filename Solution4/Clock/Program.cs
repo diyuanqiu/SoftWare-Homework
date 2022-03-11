bce585d6d59b8f3871c62d9d8b81e3bf5b3daa07
diyuanqiu <74194434+diyuanqiu@users.noreply.github.com>
2022-03-11 12:04:05 +0800

    Clock and Linked List

---
+++ Solution4/Clock/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Threading;
+namespace clock{
+    public delegate void ClockHandler(object sender, ClockEventargs args);
+    public class ClockEventargs{
+        public int hour{ get; set; }
+        public int minute{ get; set; }
+        public int second{ get; set; }
+    }
+    public class Clock
+    {
+        public event ClockHandler OnTick;
+        public event ClockHandler OnAlarm;
+        public int Hour, Minute, Second, alarmHour, alarmMinute;
+        bool runClock=true;
+        public Clock(int hour,int minute,int second){
+            if(hour<=23&&hour>=0&&minute<=59&&minute>=0&&second<=59&&second>=0){
+                this.Hour = hour;
+                this.Minute = minute;
+                this.Second = second;
+            }
+            else
+                throw new ArgumentOutOfRangeException("时钟时间错误");
+        }
+        public void setAlarm(int hour,int minute){
+            if(hour<=23&&hour>=0&&minute<=59&&minute>=0){
+                this.alarmHour = hour;
+                this.alarmMinute = minute;
+            }
+            else
+                throw new ArgumentOutOfRangeException("闹钟设置错误");
+        }
+        public void turnoffClock(){
+            runClock = false;
+        }
+        public void TickTok(){
+            while(runClock){
+                if(this.Second<=59){
+                    this.Second++;
+                }
+                else{
+                    this.Minute++;
+                    this.Second = 0;
+                }
+                if(this.Minute>=60){
+                    this.Minute = 0;
+                    this.Hour = (this.Hour++) % 24;
+                }
+                ClockEventargs args = new ClockEventargs() { hour = Hour, minute = Minute, second = Second };
+                if(this.Hour==this.alarmHour&&this.Minute==this.alarmMinute&&this.Second==0){
+                    OnAlarm(this,args);
+                    runClock = false;
+                }
+                else
+                    OnTick(this,args);
+                Thread.Sleep(1000);
+            }
+        }
+        public class NewClock{
+            public Clock newClock;
+            public NewClock(int hour,int minute,int second){
+                newClock = new Clock(hour, minute, second);
+                newClock.OnTick += this.OnTick;
+                newClock.OnAlarm += this.OnAlarm;
+            }
+            public void OnTick(object sender,ClockEventargs args){
+                Console.WriteLine("Tick Tok!");
+            }
+            public void OnAlarm(object sender,ClockEventargs args){
+                Console.WriteLine("RingRingRing!");
+            }
+        }
+        public class Program{
+            static void Main(string[] args){
+                try{
+                    NewClock myClock = new NewClock(12, 30, 0);
+                    myClock.newClock.setAlarm(12, 32);
+                    myClock.newClock.TickTok();
+                }
+                catch(ArgumentOutOfRangeException e){
+                    Console.WriteLine("时间设置出错了");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Solution4/LinkedList/LinkedList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+namespace LinkedList{
+    public class Node<T>{
+        public Node<T> Next{ get; set; }
+        public T Data{ get; set; }
+        public Node(T t){
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>{
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList(){
+            tail = head = null;
+        }
+        public Node<T> Head{
+            get => head;
+        }
+        public void Add(T t){
+            Node<T> newNode = new Node<T>(t);
+            if(tail==null){
+                head = tail = newNode;
+            }
+            else{
+                tail.Next = newNode;
+                tail = newNode;
+            }
+        }
+        public void Foreach(Action<T> action){
+            Node<T> start = this.head;
+            while(start!=null){
+                action(start.Data);
+                start = start.Next;
+            }
+        }
+    }
+    public class LinkedList{
+        static void Main(String[] args){
+            GenericList<int> linkedlist = new GenericList<int>();
+            int sum = 0;
+            int max = 0;
+            int min = 100;
+            for (int i = 0; i < 5; i++){
+                linkedlist.Add(i);
+            }
+            linkedlist.Foreach(m => Console.WriteLine("The data of the linked list is {0}", m));
+            linkedlist.Foreach(m => sum += m);
+            linkedlist.Foreach(m => { if (m > max) max = m; });
+            linkedlist.Foreach(m => { if (m < min) min = m; });
+            Console.WriteLine("The sum of the linked list is {0}", sum);
+            Console.WriteLine("The max of the linked list is {0}", max);
+            Console.WriteLine("The min of the linked list is {0}", min);
+            Console.ReadKey();
+        }
+    }
+}
