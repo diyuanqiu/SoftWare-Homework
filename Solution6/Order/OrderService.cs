bb3a786c826fe93b1c26cf5e16ac1f454ac098f3
diyuanqiu <74194434+diyuanqiu@users.noreply.github.com>
2022-03-25 20:23:58 +0800

    Sixth Homework

---
+++ Solution6/Order/OrderService.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Order
+{
+    public class OrderService
+    {
+        private List<Order> orders = new List<Order>();
+        public OrderService() { }
+        public void sortOrder()
+        {
+            orders.Sort((p1, p2) => p1.orderNo - p2.orderNo);
+        }
+        public void addOrder(Order order)
+        {
+            if (order == null)
+            {
+                throw new Exception("The adding order is null.");
+            }
+            orders.Add(order);
+        }
+        public void deleteOrder(Order order) // int orderNo
+        {
+            try
+            {
+                orders.Remove(order);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process of Delete Order is wrong.");
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void changeOrder(Order newOrder)
+        {
+            try
+            {
+                foreach (Order order in orders)
+                {
+                    if (order.orderNo == newOrder.orderNo)
+                    {
+                        orders.Remove(order);
+                        orders.Add(newOrder);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process of Change Order is wrong.");
+                Console.WriteLine(e.Message);
+            }
+        }
+        public List<Order> queryByOrderNo(int orderNo)
+        {
+            var query = from s in orders
+                        where s.orderNo == orderNo
+                        orderby s.orderNo
+                        select s;
+            return query.ToList();
+        }
+        public List<Order> queryByGoodName(string name)
+        {
+            var query = from s in orders
+                        from d in s.orderDetails
+                        where d.orderedGood.goodName == name
+                        orderby s.orderNo
+                        select s;
+            return query.ToList();
+        }
+        public List<Order> queryByClient(string name)
+        {
+            var query = from s in orders
+                        where s.client.clientName == name
+                        orderby s.orderNo
+                        select s;
+            return query.ToList();
+        }
+        public List<Order> queryByOrderPrice(string price)
+        {
+            var query = from s in orders
+                        where s.orderPrice == int.Parse(price)
+                        orderby s.orderNo
+                        select s;
+            return query.ToList();
+        }
+        public void Export()
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Order>));
+            using (FileStream fs = new FileStream("Orders.xml", FileMode.Create))
+            {
+                xmlSerializer.Serialize(fs, orders);
+            }
+            Console.WriteLine("\nSerialized as XML:");
+            Console.WriteLine(File.ReadAllText("Orders.xml"));
+        }
+        public void Import()
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Order>));
+            using (FileStream fs = new FileStream("Orders.xml", FileMode.Open))
+            {
+                orders = (List<Order>)xmlSerializer.Deserialize(fs);
+                Console.WriteLine("\nDeserialized from Orders.xml");
+            }
+        }
+    }
+}
---
+++ Solution6/OrderTests/OrderServiceTests.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Tests
+{
+    [TestClass()]
+    public class OrderServiceTests
+    {
+        OrderService service = new OrderService();
+        [TestMethod()]
+        public void sortOrderTest()
+        {
+            Client client = new Client("Diyuan");
+            Good game1 = new Good("Death cell", 38);
+            Good game2 = new Good("Empty Knight", 78);
+            Good game3 = new Good("Elda halo", 199);
+            Good game4 = new Good("GTA V", 99);
+            Good game5 = new Good("Gmod", 28);
+            OrderDetails gameDetail1 = new OrderDetails(game1, 3);
+            OrderDetails gameDetail2 = new OrderDetails(game2, 2);
+            OrderDetails gameDetail3 = new OrderDetails(game3, 5);
+            OrderDetails gameDetail4 = new OrderDetails(game4, 6);
+            OrderDetails[] Detail1 = { gameDetail1, gameDetail2, gameDetail3, gameDetail4 };
+            OrderDetails[] Detail2 = { gameDetail1, gameDetail3 };
+            OrderDetails[] Detail3 = { gameDetail2, gameDetail4 };
+            Order order1 = new Order(client, 1, Detail1);
+            Order order2 = new Order(client, 1, Detail2);
+            Order order3 = new Order(client, 3, Detail3);
+            service.addOrder(order1);
+            service.addOrder(order2);
+            service.addOrder(order3);
+            service.sortOrder();
+        }
+
+        [TestMethod()]
+        public void addOrderTest()
+        {
+            Client client = new Client("Diyuan");
+            Good game1 = new Good("Death cell", 38);
+            Good game2 = new Good("Empty Knight", 78);
+            OrderDetails gameDetail1 = new OrderDetails(game1, 3);
+            OrderDetails gameDetail2 = new OrderDetails(game2, 2);
+            OrderDetails[] Detail1 = { gameDetail1, gameDetail2 };
+            Order order1 = new Order(client, 1, Detail1);
+            service.addOrder(order1);
+        }
+
+        [TestMethod()]
+        public void deleteOrderTest()
+        {
+            Client client = new Client("Diyuan");
+            Good game1 = new Good("Death cell", 38);
+            Good game2 = new Good("Empty Knight", 78);
+            Good game3 = new Good("Elda halo", 199);
+            Good game4 = new Good("GTA V", 99);
+            Good game5 = new Good("Gmod", 28);
+            OrderDetails gameDetail1 = new OrderDetails(game1, 3);
+            OrderDetails gameDetail2 = new OrderDetails(game2, 2);
+            OrderDetails gameDetail3 = new OrderDetails(game3, 5);
+            OrderDetails gameDetail4 = new OrderDetails(game4, 6);
+            OrderDetails[] Detail1 = { gameDetail1, gameDetail2, gameDetail3, gameDetail4 };
+            OrderDetails[] Detail2 = { gameDetail1, gameDetail3 };
+            Order order1 = new Order(client, 1, Detail1);
+            Order order2 = new Order(client, 2, Detail2);
+            service.addOrder(order1);
+            service.addOrder(order2);
+            service.deleteOrder(order2);
+        }
+
+        [TestMethod()]
+        public void changeOrderTest()
+        {
+            Client client = new Client("Diyuan");
+            Good game1 = new Good("Death cell", 38);
+            Good game2 = new Good("Empty Knight", 78);
+            Good game3 = new Good("Elda halo", 199);
+            Good game4 = new Good("GTA V", 99);
+            Good game5 = new Good("Gmod", 28);
+            OrderDetails gameDetail1 = new OrderDetails(game1, 3);
+            OrderDetails gameDetail2 = new OrderDetails(game2, 2);
+            OrderDetails gameDetail3 = new OrderDetails(game3, 5);
+            OrderDetails gameDetail4 = new OrderDetails(game4, 6);
+            OrderDetails[] Detail1 = { gameDetail1, gameDetail2, gameDetail3, gameDetail4 };
+            OrderDetails[] Detail2 = { gameDetail1, gameDetail3 };
+            Order order1 = new Order(client, 1, Detail1);
+            Order order2 = new Order(client, 1, Detail2);
+            service.addOrder(order1);
+            service.changeOrder(order2);
+        }
+    }
+}
